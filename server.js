bfe2d4411f594edf14fd91381f3a79693495b2ab
Keishon Smith <keishon104@gmail.com>
2018-09-16 13:20:43 -0400

    adds new front end

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-
+app.use(express.static(__dirname + '/client'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
 
 app.listen(3000, () => {
   console.log("Your server is now running");
+
+
 
 });
